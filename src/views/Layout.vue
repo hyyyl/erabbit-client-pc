01341f680a6abd79a8fedaf1173275f3634d02e3
Ayaka <1039459965@qq.com>
2022-08-28 20:00:01 +0800

    完成了吸顶头部组件，引入了vueuse库

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,6 +3,8 @@
   <app-navbar></app-navbar>
 <!-- 头部组件 -->
   <app-header></app-header>
+  <!-- 吸顶头部组件 -->
+  <app-header-sticky></app-header-sticky>
 <!-- 内容容器 -->
   <div class="app-body">
     <router-view></router-view>
@@ -16,13 +18,15 @@ import AppNavbar from '@/components/app-navbar'
 import AppHeader from '@/components/app-header'
 import { useStore } from 'vuex'
 import AppFooter from '@/components/app-footer'
+import AppHeaderSticky from '@/components/app-header-sticky'
 
 export default {
   name: 'Layout',
   components: {
     AppNavbar,
     AppHeader,
-    AppFooter
+    AppFooter,
+    AppHeaderSticky
   },
   setup () {
     const store = useStore()
